3a604be3c6b4371f6759cd7797643b7e92f688ea
voroboosheque <voroboosheque@yandex.com>
2015-02-22 15:29:27 +0300

    created Xcode project

---
+++ vorobooshequeTests/vorobooshequeTests.m
@@ -0,0 +1,40 @@
+//
+//  vorobooshequeTests.m
+//  vorobooshequeTests
+//
+//  Created by admin on 22/02/15.
+//  Copyright (c) 2015 voroboosheque. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface vorobooshequeTests : XCTestCase
+
+@end
+
+@implementation vorobooshequeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
